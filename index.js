9e2f94d03fe7037931a61f00957bec508a55e7c9
AmandaMS09 <mandanevs@gmail.com>
2023-01-21 19:38:07 -0300

    Troca de serviços - remoção do firebase-admin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 app.use(
   cors({
@@ -15,15 +15,21 @@ app.use(
 );
 
 // Configuração do cloud firestore
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-
-initializeApp();
-
-const db = getFirestore();
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Configuração do meu app no firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD71Lo8v9xiHluEkT21tau8v14rHiFwTBg",
+  authDomain: "web-livro-compartilhado.firebaseapp.com",
+  projectId: "web-livro-compartilhado",
+  storageBucket: "web-livro-compartilhado.appspot.com",
+  messagingSenderId: "832168917364",
+  appId: "1:832168917364:web:6ed72fc9cecbea19a35421",
+  measurementId: "G-L5KJY4J17R"
+};
+// Inicialização do firebase
+const firebase = initializeApp(firebaseConfig);
+// Inicialização do cloud firestore
+const db = getFirestore(firebase);
 
 // Rotas
-
-app.get("/", function(req, res) {
-  res.sendFile("public/index.html")
-})
